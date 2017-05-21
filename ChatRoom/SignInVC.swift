7c7274fd22d54905496fa3d617db428ccfaef018
topdude15 <topdude15@gmail.com>
2017-05-21 17:04:59 -0400

    Initial setup complete - Pods installed and started work on sign up screen

---
+++ ChatRoom/SignInVC.swift
@@ -0,0 +1,37 @@
+//
+//  SignInVC.swift
+//  ChatRoom
+//
+//  Created by Trevor Rose on 5/21/17.
+//  Copyright © 2017 Trevor Rose. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func resignKeyboard(sender: AnyObject) {
+        _ = sender.resignFirstResponder()
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,26 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '9.0'
+
+target 'ChatRoom' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ChatRoom
+  pod 'Firebase'
+  pod 'Firebase/Database'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Storage'
+  pod 'Firebase/AdMob'
+  pod 'SwiftKeychainWrapper'
+
+  target 'ChatRoomTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ChatRoomUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
